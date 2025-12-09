1eb7511dc7b1223232e8a4597ad884ce4faf7d9a
SubramanyamViewplus <subramanyam@viewplus.ie>
2025-12-09 13:17:39 +0000

    Removed Bg-white for ServiceCard

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -7,7 +7,7 @@ interface ServiceCardProps {
 
 const ServiceCard: React.FC<ServiceCardProps> = ({ title, description }) => {
   return (
-<div className="border rounded-lg p-6 shadow hover:shadow-2xl hover:scale-105 transition transform duration-300 bg-white">
+<div className="border rounded-lg p-6 shadow hover:shadow-2xl hover:scale-105 transition transform duration-300">
       <h3 className="text-xl font-bold mb-2">{title}</h3>
       <p>{description}</p>
     </div>
