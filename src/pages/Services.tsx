07c60eab00a68cf4b28e4ba59597bce17b6a920f
SubramanyamViewplus <subramanyam@viewplus.ie>
2025-12-09 13:11:00 +0000

    Fixes

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BriefcaseIcon, HomeIcon, TruckIcon } from "@heroicons/react/24/outline";
-import ServiceCard from "../components/ServiceCard";
 
 const services = [
   {
